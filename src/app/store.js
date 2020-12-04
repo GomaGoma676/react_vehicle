5f2dc56528e2c30ccf3195f3a5e64d470774ff87
GomaGoma676 <pga.adam.scott@gmail.com>
2020-12-04 19:00:43 +0900

    initial commit

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
+import vehicleReducer from "../features/vehicleSlice";
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    auth: authReducer,
+    vehicle: vehicleReducer,
   },
 });
